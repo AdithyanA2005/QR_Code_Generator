4f5e31b73bcce8b42ae14358de5840836f7b0bcc
Adithyan A <deepaadithyan56@gmail.com>
2021-04-02 17:01:38 +0530

    These are some of the config list and tuples

---
+++ generator/settings.py
@@ -0,0 +1,13 @@
+# These are the words that must be removed from the qrcode filename
+name_remove_list = [
+    'https://',
+    'http://',
+    '.com'
+]
+
+# These are the configuration data
+config = {
+    'qr_dir': 'generator/static/temporary/',
+    'qr_brand': 'QRG_',
+    'qr_extention': '.png'
+}
